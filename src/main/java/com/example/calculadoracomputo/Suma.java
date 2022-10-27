0f49fbdc0ef6d7193ec1ab7c42d94ee41d9c4c2f
inaki321 <inakihe00@gmail.com>
2022-10-27 18:34:22 -0500

    delete server file

--- src/main/java/com/example/calculadoracomputo/Suma.java
+++
@@ -1,9 +0,0 @@
-package com.example.calculadoracomputo;
-
-public class Suma {
-    public float SumaGet(float n1, float n2) {
-        System.out.println("Numero 1 recibido "+n1);
-        System.out.println("Numero 2 recibido "+n2);
-        return n1 + n2;
-    }
-}
